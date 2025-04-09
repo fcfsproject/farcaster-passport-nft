ae9fd4f6a8ab5c9a269ffffdfa0ab602cf2efe35
FCFS project <moxienism@gmail.com>
2025-04-09 20:11:00 +0700

    desai awal

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# farcaster-passport-nft
+# Farcaster Passport NFT
+
+Generate and mint a digital passport NFT from your Farcaster profile.
---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", paddingTop: 100 }}>
+      <h1>Farcaster Passport NFT</h1>
+      <p>Coming soon: auto-generate your digital NFT passport from your Farcaster profile.</p>
+    </div>
+  );
+}
